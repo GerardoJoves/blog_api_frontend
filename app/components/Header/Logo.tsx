c579a14ab0d28dbb1391a721636fff398250eb45
Gerardo <gerardooenrrique@gmail.com>
2025-08-05 22:17:06 -0400

    feat: update header logo

--- app/components/Header/Logo.tsx
+++ app/components/Header/Logo.tsx
@@ -1,10 +1,12 @@
+import { LibraryBig } from 'lucide-react';
 import { Link } from 'react-router';
 
 export default function Logo() {
   return (
     <Link to="/">
-      <div className="h-12 flex items-center text-xl font-bold tracking-wide">
-        Thought Flow
+      <div className="h-12 flex items-center gap-2">
+        <LibraryBig />
+        <div className="text-xl font-bold tracking-wide">Blogging</div>
       </div>
     </Link>
   );
