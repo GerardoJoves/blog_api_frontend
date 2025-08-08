8fc8c813bb88b31321ce9a97046de66de864abd1
Gerardo <gerardooenrrique@gmail.com>
2025-08-08 09:25:24 -0400

    refactor: Use semantic footer tag in Footer component

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import GithubDark from './icons/github_dark.svg?react';
 
 export default function Footer() {
   return (
-    <div className="px-4 sm:px-10 lg:px-14 pt-18 border-t border-neutral-400 dark:border-gray-700">
+    <footer className="px-4 sm:px-10 lg:px-14 pt-18 border-t border-neutral-400 dark:border-gray-700">
       <div className="flex flex-wrap justify-between gap-x-14 gap-y-8">
         <div>
           <PoweredBy />
@@ -29,6 +29,6 @@ export default function Footer() {
           </a>
         </p>
       </div>
-    </div>
+    </footer>
   );
 }
