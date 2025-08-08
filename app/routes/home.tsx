1860eac9aa53d25b571cd9fdb5cc793a7f2babdd
Gerardo <gerardooenrrique@gmail.com>
2025-08-08 08:36:06 -0400

    refactor: Move home page hero section inside main tag

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -19,20 +19,22 @@ export default function Home({ loaderData }: Route.ComponentProps) {
   return (
     <>
       <title>Blogging</title>
-      <div className="mb-20">
-        <Hero />
-      </div>
-      <main className="px-4 sm:px-10 lg:px-14 mb-20">
-        <div className="flex justify-between items-center mb-8">
-          <h1 className="text-2xl font-bold">Recent Posts</h1>
-          <Link to="/posts">
-            <div className="flex gap-1 items-center text-sm sm:text-base hover:underline">
-              <span>View all posts</span>
-              <ChevronRight size={18} />
-            </div>
-          </Link>
+      <main>
+        <div className="mb-20">
+          <Hero />
         </div>
-        <PostsContainer posts={latestPosts} />
+        <div className="px-4 sm:px-10 lg:px-14 mb-20">
+          <div className="flex justify-between items-center mb-8">
+            <h1 className="text-2xl font-bold">Recent Posts</h1>
+            <Link to="/posts">
+              <div className="flex gap-1 items-center text-sm sm:text-base hover:underline">
+                <span>View all posts</span>
+                <ChevronRight size={18} />
+              </div>
+            </Link>
+          </div>
+          <PostsContainer posts={latestPosts} />
+        </div>
       </main>
     </>
   );
