a7bfa83f0ebc684d97020607dee5030a4c737a00
Gerardo <gerardooenrrique@gmail.com>
2025-08-20 15:08:19 -0400

    feat: Add theme preference cookie

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import * as z from 'zod';
+import serverCookie from 'cookie';
+import clientCookie from 'js-cookie';
 import {
   isRouteErrorResponse,
   Links,
@@ -6,6 +9,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  data,
+  useLoaderData,
 } from 'react-router';
 
 import type { Route } from './+types/root';
@@ -25,13 +30,34 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
-type ThemeType = 'light' | 'dark';
+const themeSchema = z.union([z.literal('light'), z.literal('dark')]);
+type ThemeType = z.infer<typeof themeSchema>;
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const cookieHeader = request.headers.get('Cookie') ?? '';
+  const cookies = serverCookie.parse(cookieHeader);
+  const theme = themeSchema.safeParse(cookies.theme);
+  if (theme.error) {
+    const cookie = serverCookie.serialize('theme', 'light', {
+      maxAge: 31_536_000, // 1 year
+      httpOnly: false,
+    });
+    return data<{ theme: ThemeType }>(
+      { theme: 'light' },
+      { headers: { 'Set-Cookie': cookie } },
+    );
+  }
+  return data<{ theme: ThemeType }>({ theme: theme.data });
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState<ThemeType>('light');
+  const loaderData = useLoaderData<typeof loader>();
+  const [theme, setTheme] = useState<ThemeType>(loaderData.theme);
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+    clientCookie.set('theme', nextTheme, { expires: 365, path: '/' });
   };
 
   return (
